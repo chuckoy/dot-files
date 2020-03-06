c475c0683eda01a2d8063b4defc554072caf8781
Chuck Logan Lim <chucklogan.lim@gmail.com>
2020-03-06 15:58:16 +1100

    Indent link.sh echos and initiate nvim init file

--- install/link.sh
+++ install/link.sh
@@ -7,9 +7,14 @@ links=$(find -H "$DF" -maxdepth 3 -name '*.symlink')
 for f in $links; do 
     t="$HOME/.$(basename $f ".symlink")"
     if [ -e $t ]; then
-        echo "~${t#$HOME} already exists; skipping..."
+        printf "\t"
+        echo "👺 ~${t#$HOME} already exists; skipping..."
     else
-        echo "Creating symlink for $t..."
+        printf "\t"
+        echo "👺 Creating symlink for $t..."
         ln -s $f $t
     fi 
 done
+
+echo "𝒱 Linking neovim to vim..."
+cat $DF/.config/nvim/init.vim > $HOME/.config/nvim/init.vim
