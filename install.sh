51ea1e2d666e84757ac4508473a42244992fc66b
Chuck Logan Lim <chuck.lim@safetyculture.io>
2020-03-06 15:07:36 +1100

    Add link.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "👺 Starting installation... 👺"
+
+source ./install/link.sh
 source ./install/brew.sh
 source ./install/osx.sh
 
 echo "👺 Done installing! 👺"
-
+exit 0
---
+++ install/link.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+DF=$HOME/dot-files
+
+echo "👺 Creating symlinks..."
+links=$(find -H "$DF" -maxdepth 3 -name '*.symlink')
+for f in $links; do 
+    t="$HOME/.$(basename $f ".symlink")"
+    if [ -e $t ]; then
+        echo "~${t#$HOME} already exists; skipping..."
+    else
+        echo "Creating symlink for $t..."
+        ln -s $f $t
+    fi 
+done
