79e584d86c6d63d0b76db09e0bfe2f9dfd8845f2
Chuck Logan Lim <chucklogan.lim@gmail.com>
2021-05-06 11:10:24 +1000

    Add comments to brew install packages

--- install/brew.sh
+++ install/brew.sh
@@ -11,22 +11,23 @@ else
 fi
 
 echo "👺 Installing Homebrew packages..."
-brew install git
-brew install go
-brew install node
-brew install python
-brew install vim
-brew install neovim
-brew install nvm
+brew install git    # keep git updated with homebrew
+brew install go     # Go programming language
+brew install node   # Node.js
+brew install python # Python programming language
+brew install vim    # standard vim
+brew install neovim # new vim
+brew install nvm    # Node.js version manager
 
 echo "👺 Installing nice casks..."
-brew install --cask visual-studio-code
-brew install --cask firefox
-brew install --cask iterm2
-brew install --cask slack
-brew install --cask spotify
-brew install --cask docker
-brew install --cask 1password
+brew install --cask firefox             # internet browser
+brew install --cask visual-studio-code  # code editor
+brew install --cask iterm2              # better terminal
+brew install --cask slack               # messaging client
+brew install --cask spotify             # music client
+brew install --cask docker              # *nix containers
+brew install --cask 1password           # password manager
+brew install --cask wombat              # gRPC GUI client
 
 echo "👺 Cleanup..."
 brew cleanup
