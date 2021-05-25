830035163b362025617c73c550ca36b585ce0476
Chuck Logan Lim <chucklogan.lim@gmail.com>
2021-05-25 13:21:36 +1000

    Fix names of applications to show on Dock

--- install/osx.sh
+++ install/osx.sh
@@ -24,10 +24,10 @@ defaults write com.apple.dock persistent-apps -array
 
 echo "🍎 Dock: add default app icons"
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Firefox.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual%20Studio%20Code.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Visual Studio Code.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Slack.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
-defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Authy%20Desktop.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
+defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Authy Desktop.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Discord.app/</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>";
 
 echo "🍎 iTerm2: specify the preferences directory"
