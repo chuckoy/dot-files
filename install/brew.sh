98a41cddc6c93699d668831bf220cc38f7237263
Chuck Logan Lim <chucklogan.lim@gmail.com>
2022-10-20 13:20:27 +1100

    Remove python, viscosity, and spotify

--- install/brew.sh
+++ install/brew.sh
@@ -14,7 +14,6 @@ echo "👺 Installing Homebrew packages..."
 brew install git    # keep git updated with homebrew
 brew install go     # Go programming language
 brew install node   # Node.js
-brew install python # Python programming language
 brew install vim    # standard vim
 brew install neovim # new vim
 brew install nvm    # Node.js version manager
@@ -24,12 +23,10 @@ brew install --cask firefox             # internet browser
 brew install --cask visual-studio-code  # code editor
 brew install --cask iterm2              # better terminal
 brew install --cask slack               # messaging client
-brew install --cask spotify             # music client
 brew install --cask docker              # *nix containers
 brew install --cask 1password           # password manager
 brew install --cask wombat              # gRPC GUI client
 brew install --cask karabiner-elements  # Keyboard customiser
-brew install --cask viscosity           # VPN client
 brew install --cask discord             # you know Discord
 brew install --cask authy               # MFA client
 brew install --cask rocket              # we want the emoji
